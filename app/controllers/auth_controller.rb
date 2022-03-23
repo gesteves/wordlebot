b08c02fd0bf818c886f0731bc571bd817a5ae415
Guillermo Esteves <g@gesteves.com>
2022-03-22 21:17:16 -0600

    Add some rudimentary error handling

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,13 +3,18 @@ class AuthController < ApplicationController
     if params[:code].present?
       slack = Slack.new
       token = slack.get_access_token(code: params[:code], redirect_uri: auth_url)
-      if token[:ok].present?
+      if token[:ok]
         access_token = token[:access_token]
         channel_id = token.dig(:incoming_webhook, :channel_id)
-        slack.conversation_join(channel_id: channel_id, access_token: access_token)
-        notice ='Wordlebot has been added to your Slack. Yay!'
+        join = slack.conversation_join(channel_id: channel_id, access_token: access_token)
+        if join[:ok]
+          notice = 'Wordlebot was added to your Slack. Yay!'
+          # TODO: Store token, webhook, etc.
+        else
+          notice = "Wordlebot was not added to your Slack for the following reason: “#{join[:error]}”. Boo!"
+        end
       else
-        notice = 'Authentication failed. Try again!'
+        notice = "Authentication failed for the following reason: “#{token[:error]}”. Boo!"
       end
     else
       notice = 'Authentication failed. Try again!'
