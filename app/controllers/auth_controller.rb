64a189ab46b361b50b527c2d53d5bbabb07d1384
Guillermo Esteves <g@gesteves.com>
2022-03-23 09:05:55 -0600

    Tweak

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -7,10 +7,9 @@ class AuthController < ApplicationController
         access_token = token[:access_token]
         team_id = token.dig(:team, :id)
         notice = 'Wordlebot was added to your Slack. Yay!'
-        team = Team.find_or_create_by!(team_id: team_id) do |t|
-          t.access_token = access_token
-        end
-        notice = team.present? ? 'Wordlebot was added to your Slack. Yay!' : 'Authentication failed. Try again!'
+        team = Team.find_or_create_by(team_id: team_id)
+        team.access_token = access_token
+        notice = team.save ? 'Wordlebot was added to your Slack. Yay!' : 'Authentication failed. Try again!'
       else
         notice = "Authentication failed for the following reason: “#{token[:error]}”. Boo!"
       end
