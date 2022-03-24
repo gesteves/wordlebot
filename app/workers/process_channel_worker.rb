930ab467b74c3dc6a115956de59683b42836619b
Guillermo Esteves <g@gesteves.com>
2022-03-24 15:38:57 -0600

    Post results in channel

--- app/workers/process_channel_worker.rb
+++ app/workers/process_channel_worker.rb
@@ -2,6 +2,11 @@ class ProcessChannelWorker < ApplicationWorker
   def perform(team_id, channel_id)
     team = Team.find(team_id)
     scores = team.wordle_scores_in_channel(channel_id: channel_id, game_number: Wordle.yesterdays_game)
-    stats = Wordle.stats(scores)
+
+    text = "Results for Wordle #{Wordle.yesterdays_game}"
+    blocks = Wordle.to_slack_blocks(game_number: Wordle.yesterdays_game, scores: scores)
+
+    team.post_in_channel(channel_id: channel_id, text: text, blocks: blocks)
   end
+
 end
