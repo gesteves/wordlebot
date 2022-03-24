9909957859faab80a7d7da140fcaf96172fa51d3
Guillermo Esteves <g@gesteves.com>
2022-03-24 15:38:26 -0600

    Expand post message method

--- app/lib/slack.rb
+++ app/lib/slack.rb
@@ -8,24 +8,7 @@ class Slack
   # @param code [String] A temporary authorization code granted by OAuth
   # @param redirect_uri [String] The redirect URI specified in the initial auth request
   # @see https://api.slack.com/methods/oauth.v2.access
-  # @return [String] A JSON response like this:
-  # {
-  #   "ok": true,
-  #   "app_id": "XXXXX",
-  #   "authed_user": {
-  #       "id": "XXXXX"
-  #   },
-  #   "scope": "incoming-webhook,groups:history,channels:history,channels:join",
-  #   "token_type": "bot",
-  #   "access_token": "xoxb-XXXXX,
-  #   "bot_user_id": "XXXXX",
-  #   "team": {
-  #       "id": "XXXXX",
-  #       "name": "The Team Name"
-  #   },
-  #   "enterprise": null,
-  #   "is_enterprise_install": false
-  # }
+  # @return [String] A JSON response.
   def get_access_token(code:, redirect_uri: nil)
     query = {
       code: code,
@@ -47,7 +30,7 @@ class Slack
   # @param latest [String] End of time range of messages to include in results. Default is the current time.
   # @param oldest [String] Start of time range of messages to include in results.
   # @see https://api.slack.com/methods/conversations.history
-  # @return [String] A JSON response
+  # @return [String] A JSON response.
   def conversation_history(channel_id:, access_token:, cursor: nil, inclusive: false, limit: 1000, latest: nil, oldest: nil )
     query = {
       channel: channel_id,
@@ -71,7 +54,7 @@ class Slack
   # @param limit [Integer] The maximum number of items to return, no larger than 1000.
   # @param cursor [String] Used to paginate through collections of data.
   # @see https://api.slack.com/methods/conversations.list
-  # @return [String] A JSON response
+  # @return [String] A JSON response.
   def conversations_list(access_token:, team_id:, types: 'public_channel,private_channel', exclude_archived: true, limit: 1000, cursor: nil)
     query = {
       team_id: team_id,
@@ -88,14 +71,50 @@ class Slack
 
   # Sends a message to a channel.
   # @param access_token [String] Authentication token bearing required scopes.
+  # @param channel_id [String] Channel, private group, or IM channel to send message to.
+  # @param attachments [Hash] The content of the message.
+  # @param blocks [Hash] The content of the message.
+  # @param text [String] The content of the message. If `attachments`` or `blocks`` are included, `text`` will be used as fallback text for notifications only.
+  # @param link_names [Boolean] Find and link channel names and usernames.
+  # @param markdown [Boolean] Disable Slack markup parsing by setting to false.
+  # @param parse [String] Change how messages are treated, can be `none` or `full`.
+  # @param reply_broadcast [Boolean] Used in conjunction with `thread_ts`` and indicates whether reply should be made visible to everyone in the channel or conversation.
+  # @param thread_ts [String] Provide another message's ts value to make this message a reply.
+  # @param unfurl_links [Boolean] Pass true to enable unfurling of primarily text-based content
+  # @param unfurl_media [Boolean] Pass false to disable unfurling of media content.
   # @see https://api.slack.com/methods/chat.postMessage
-  # @return [String] A JSON response
-  def post_message(access_token:, text:)
+  # @return [String] A JSON response.
+  def post_message(
+    access_token:,
+    channel_id:,
+    attachments: nil,
+    blocks: nil,
+    text: nil,
+    link_names: true,
+    markdown: true,
+    parse: 'none',
+    reply_broadcast: false,
+    thread_ts: nil,
+    unfurl_links: true,
+    unfurl_media: true
+  )
+    return if attachments.blank? && blocks.blank? && text.blank?
+    params = {
+      channel: channel_id,
+      attachments: attachments,
+      blocks: blocks,
+      text: text,
+      link_names: link_names,
+      mrkdwn: markdown,
+      parse: 'none',
+      reply_broadcast: false,
+      thread_ts: nil,
+      unfurl_links: true,
+      unfurl_media: true
+    }.compact
     response = HTTParty.post("https://slack.com/api/chat.postMessage",
-                            body: {
-                              text: text
-                            }.to_json,
-                            headers: { 'Authorization': "Bearer #{access_token}" })
+                            body: params.to_json,
+                            headers: { 'Authorization': "Bearer #{access_token}", 'Content-Type': 'application/json' })
     JSON.parse(response.body, symbolize_names: true)
   end
 end
