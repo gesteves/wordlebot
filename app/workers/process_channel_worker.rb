9fca7eeb2692a8c62e49a74aa21de2b6c9ee76bb
Guillermo Esteves <g@gesteves.com>
2022-03-24 16:31:27 -0600

    Don't send messages if there are no scores

--- app/workers/process_channel_worker.rb
+++ app/workers/process_channel_worker.rb
@@ -2,6 +2,7 @@ class ProcessChannelWorker < ApplicationWorker
   def perform(team_id, channel_id)
     team = Team.find(team_id)
     scores = team.wordle_scores_in_channel(channel_id: channel_id, game_number: Wordle.yesterdays_game)
+    return if scores.blank?
 
     text = "Results for Wordle #{Wordle.yesterdays_game}"
     blocks = Wordle.to_slack_blocks(game_number: Wordle.yesterdays_game, scores: scores)
