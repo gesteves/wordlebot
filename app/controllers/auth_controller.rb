0d239c0a1b91e6278ece07293ae10d802acf985b
Guillermo Esteves <g@gesteves.com>
2022-03-28 16:08:30 -0600

    Fix double render error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,7 @@ class AuthController < ApplicationController
     if params[:code].present?
       slack = Slack.new
       token = slack.get_access_token(code: params[:code], redirect_uri: auth_url)
+      url = root_url
       if token[:ok]
         access_token = token[:access_token]
         team_id = token.dig(:team, :id)
@@ -10,7 +11,8 @@ class AuthController < ApplicationController
         team.access_token = access_token
         if team.save
           logger.info "Team #{team_id} authenticated with the following scopes: #{token[:scope]}"
-          redirect_to success_url
+          notice = nil
+          url = success_url
         else
           notice = 'Oh no, something went wrong. Please try again!'
         end
@@ -22,6 +24,6 @@ class AuthController < ApplicationController
       logger.error "Authentication failed for the following reason: #{params[:error]}"
       notice = "Wordlebot was not added to your Slack. Please try again!"
     end
-    redirect_to root_url, notice: notice
+    redirect_to url, notice: notice
   end
 end
