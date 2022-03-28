82a5b6d7b0f69be116e63f35b0b36134a2ed320c
Guillermo Esteves <g@gesteves.com>
2022-03-28 07:29:08 -0600

    Remove debug

--- app/models/team.rb
+++ app/models/team.rb
@@ -86,14 +86,11 @@ class Team < ApplicationRecord
     image = response.dig(:user, :profile, :image_512).presence || response.dig(:user, :profile, :image_192).presence || response.dig(:user, :profile, :image_48).presence || response.dig(:user, :profile, :image_32).presence || response.dig(:user, :profile, :image_original).presence
     name = response.dig(:user, :real_name) || response.dig(:user, :name)
 
-    user = {
+    {
       text: text,
       user: user_id,
       image: image,
       name: name
     }
-
-    logger.info user if ENV['DEBUG'].present?
-    user
   end
 end
